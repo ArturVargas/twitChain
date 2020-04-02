6f151b61f79c5f9e9ca49be8ca468c4cdd4498dd
ArturVargas <arturvargas9226@gmail.com>
2020-04-01 20:54:48 -0600

    instance getWeb3 for connect with metamask

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,40 @@ import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+import Navbar from './components/Navbar';
 
+class App extends Component {
+  state = {
+    account: '',
+    twitChain: null,
+    postCount: 0,
+    posts: [],
+    loading: true
+  }
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
+      //instacia de web3
       const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
+      // regresa un array de accounts
       const accounts = await web3.eth.getAccounts();
+      console.log(accounts);
+      this.setState({ account: accounts[0] })
 
-      // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+      const deployedNetwork = TwitChain.networks[networkId];
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      if (deployedNetwork) {
+        const twitChain = new web3.eth.Contract(
+          TwitChain.abi,
+          deployedNetwork.address,
+        );
+        this.setState({ twitChain });
+        const postCount = await twitChain.methods.postCount().call()
+        this.setState({ postCount })
+
+      }
+
     } catch (error) {
-      // Catch any errors for any of the above operations.
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`,
       );
@@ -35,36 +45,12 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <Navbar account={this.state.account}/>
+
       </div>
     );
   }
